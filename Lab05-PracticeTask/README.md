8e41ae7adf1bdc3ca929b1d5edcc7e786a5fa06a
SHAN DURRANI <71522552+shandurrani428@users.noreply.github.com>
2021-11-28 11:44:24 +0500

    Add files via upload

---
+++ Lab05-PracticeTask/README.md
@@ -0,0 +1,1 @@
+# ShanDurrani_PracticeTask
---
+++ Lab05-PracticeTask/ShanDurrani_PracticeTask/src/Servlets/lab05Servlet.java
@@ -0,0 +1,21 @@
+package Servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "lab05Servlet")
+public class lab05Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pw=response.getWriter();
+        pw.write("Welcome To Servlet!");
+    }
+}
